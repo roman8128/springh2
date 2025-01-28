6c0500ed722b031ed721a955336414b1d9fe9b30
Roman <4464530+roman8128@users.noreply.github.com>
2025-01-28 15:56:01 +0100

    removed lombok for now

--- src/main/java/com/example/h2/H2Application.java
+++ src/main/java/com/example/h2/H2Application.java
@@ -1,13 +1,29 @@
 package com.example.h2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class H2Application {
 
-	public static void main(String[] args) {
+public class H2Application implements CommandLineRunner {
+	private final DataSource dataSource;
+
+    public H2Application(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(H2Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(dataSource.toString());
+		final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+		jdbcTemplate.execute("select 1");
+	}
 }
